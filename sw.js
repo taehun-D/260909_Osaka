a76fbc13d489898d22cd601659a0f31a85dfe6ae
taehun-D <pfwelfare@gmail.com>
2026-09-08 10:15:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 오프라인 캐시. 데이터나 사진을 바꾸면 아래 VERSION 숫자를 올리세요. */
-const VERSION = "osaka-v14";
+const VERSION = "osaka-v15";
 
 const CORE = [
   "./", "./index.html", "./data.js", "./manifest.json",
